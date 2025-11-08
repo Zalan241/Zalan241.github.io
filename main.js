4d42578efc670ec421f298661748e09fe9c5e0cc
zalan kovacs <zalan.dev@gmail.com>
2025-11-09 00:26:21 +0200

    Expand chart data

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ const spec1 = {
       { kateg: "A", érték: 28 },
       { kateg: "B", érték: 55 },
       { kateg: "C", érték: 43 },
-      { kateg: "D", érték: 91 }
+      { kateg: "D", érték: 91 },
+      { kateg: "E", érték: 67 },
+      { kateg: "F", érték: 52 },
+      { kateg: "G", érték: 74 },
+      { kateg: "H", érték: 39 }
     ]
   },
   params: [
@@ -20,7 +24,7 @@ const spec1 = {
     type: "bar",
     cornerRadiusTopLeft: 4,
     cornerRadiusTopRight: 4,
-    cursor: "pointer"  
+    cursor: "pointer"
   },
   encoding: {
     x: { field: "kateg", type: "nominal", title: "Kategória" },
@@ -45,12 +49,16 @@ const spec2 = {
       { dátum: "2024-01-05", érték: 12 },
       { dátum: "2024-02-01", érték: 18 },
       { dátum: "2024-02-28", érték: 24 },
-      { dátum: "2024-03-15", érték: 29 }
+      { dátum: "2024-03-15", érték: 29 },
+      { dátum: "2024-04-01", érték: 33 },
+      { dátum: "2024-04-25", érték: 37 },
+      { dátum: "2024-05-10", érték: 42 },
+      { dátum: "2024-06-01", érték: 49 }
     ]
   },
   mark: {
     type: "line",
-    point: { cursor: "pointer" }   
+    point: { cursor: "pointer" }
   },
   encoding: {
     x: { field: "dátum", type: "temporal", title: "Dátum" },
@@ -71,13 +79,19 @@ const spec3 = {
       { x: 10, y: 20 },
       { x: 15, y: 30 },
       { x: 22, y: 25 },
-      { x: 28, y: 35 }
+      { x: 28, y: 35 },
+      { x: 32, y: 40 },
+      { x: 36, y: 45 },
+      { x: 40, y: 38 },
+      { x: 45, y: 50 },
+      { x: 50, y: 47 },
+      { x: 55, y: 53 }
     ]
   },
   mark: {
     type: "circle",
     size: 150,
-    cursor: "pointer"  
+    cursor: "pointer"
   },
   encoding: {
     x: { field: "x", type: "quantitative", title: "X érték" },
@@ -89,6 +103,6 @@ const spec3 = {
   }
 };
 
-vegaEmbed("#chart1", spec1).catch(err => console.error("chart1 error:", err));
-vegaEmbed("#chart2", spec2).catch(err => console.error("chart2 error:", err));
-vegaEmbed("#chart3", spec3).catch(err => console.error("chart3 error:", err));
+vegaEmbed("#chart1", spec1);
+vegaEmbed("#chart2", spec2);
+vegaEmbed("#chart3", spec3);
